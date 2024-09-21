a631344a916e50ce708507474116434d03e73e2e
Ujjwal Katiyar <86518408+devxrithm@users.noreply.github.com>
2024-09-21 11:24:12 +0530

    Update SecHero.jsx

--- src/components/SecHero.jsx
+++ src/components/SecHero.jsx
@@ -3,50 +3,55 @@ import { Link } from "react-router-dom";
 const Sechero = () => {
     return (
         <>
-            <section id="tools" className="sec-container">
-                <h1 className="sec-head">TOOLS</h1>
-                <p className="sec-para">Discover the Convenience of Rapid Calculator: Swiftly compute ages, time spans, dates, hours, BMI, and GPA. Experience hassle-free calculations with Rapid Calculator today.</p>
-                <div className="container-1">
+            <div className="back-sec">
+                <section id="tools" className="sec-container">
+                    <h1 className="sec-head">TOOLS</h1>
+                    <p className="sec-para">Discover the Convenience of Rapid Calculator: Swiftly compute ages, time spans, dates, hours, BMI, and GPA. Experience hassle-free calculations with Rapid Calculator today.</p>
+                    <div className="container-1">
 
-                    <Link to="/about">
+                        <Link to="/age-calculation">
+                            <div className="box-x">
+                                <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/8053/8053014.png" alt="Image 1" />
+                                <p>AGE<br /> CALCULATOR</p>
+                            </div>
+                        </Link>
+
+                        <Link to="/time-duration-calculator">
+                            <div className="box-x">
+                                <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/5192/5192151.png" alt="Image 2" />
+                                <p>TIME DURATION<br /> CALCULATOR</p>
+                            </div>
+                        </Link>
                         <div className="box-x">
-                            <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/8053/8053014.png" alt="Image 1" />
-                            <p>AGE<br /> CALCULATOR</p>
+                            <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/11484/11484322.png" alt="Image 3" />
+                            <p>DATE<br /> CALCULATOR</p>
                         </div>
-                    </Link>
+                        <div className="box-x">
+                            <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/10440/10440341.png" alt="Image 4" />
+                            <p>HOUR<br /> CALCULATOR</p>
+                        </div>
+                        <Link to="/bmi-calculator">
+                            <div className="box-x">
+                                <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/3373/3373123.png" alt="Image 5" />
+                                <p>BMI<br /> CALCULATOR</p>
+                            </div>
+                        </Link>
+                        <div className="box-x">
+                            <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/2247/2247890.png" alt="Image 6" />
+                            <p>GPA<br /> CALCULATOR</p>
+                        </div>
+                        <div className="box-x">
+                            <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/2247/2247890.png" alt="Image 6" />
+                            <p>GPA<br /> CALCULATOR</p>
+                        </div>
+                        <div className="box-x">
+                            <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/2247/2247890.png" alt="Image 6" />
+                            <p>GPA<br /> CALCULATOR</p>
+                        </div>
 
-                    <div className="box-x">
-                        <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/5192/5192151.png" alt="Image 2" />
-                        <p>TIME<br /> CALCULATOR</p>
                     </div>
-                    <div className="box-x">
-                        <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/11484/11484322.png" alt="Image 3" />
-                        <p>DATE<br /> CALCULATOR</p>
-                    </div>
-                    <div className="box-x">
-                        <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/10440/10440341.png" alt="Image 4" />
-                        <p>HOUR<br /> CALCULATOR</p>
-                    </div>
-                    <div className="box-x">
-                        <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/3373/3373123.png" alt="Image 5" />
-                        <p>BMI<br /> CALCULATOR</p>
-                    </div>
-                    <div className="box-x">
-                        <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/2247/2247890.png" alt="Image 6" />
-                        <p>GPA<br /> CALCULATOR</p>
-                    </div>
-                    <div className="box-x">
-                        <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/2247/2247890.png" alt="Image 6" />
-                        <p>GPA<br /> CALCULATOR</p>
-                    </div>
-                    <div className="box-x">
-                        <img height="100" width="100" className="box-img" src="https://cdn-icons-png.flaticon.com/128/2247/2247890.png" alt="Image 6" />
-                        <p>GPA<br /> CALCULATOR</p>
-                    </div>
-
-                </div>
-            </section>
-
+                </section>
+            </div>
 
         </>
     );
