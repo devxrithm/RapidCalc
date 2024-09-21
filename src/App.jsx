a5bfa3f5d670c81df53479646a03234e1537905c
Ujjwal Katiyar <86518408+devxrithm@users.noreply.github.com>
2024-09-21 11:23:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import {
     Switch,
     Route,
 } from "react-router-dom";
+import BMICalculator from './components/BMI/Bmi';
+import Timeduration from './components/Timeduration/time';
 
 
 export default function App() {
@@ -20,8 +22,14 @@ export default function App() {
                     <Route exact path="/">
                         <Hero />
                     </Route>
-                    <Route exact path="/about">
+                    <Route exact path="/age-calculation">
                         <AgeCal />
+                    </Route>
+                    <Route exact path="/bmi-calculator">
+                        <BMICalculator />
+                    </Route>
+                    <Route exact path="/time-duration-calculator">
+                        <Timeduration/>
                     </Route>
                 </Switch>
                 <Footer />
